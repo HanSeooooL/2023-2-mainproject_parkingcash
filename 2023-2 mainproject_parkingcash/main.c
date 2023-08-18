58ea5d98d826baceea5b525020ed9ffcfe055332
HANSEOL <nothinonthere@icoud.com>
2023-08-18 23:53:45 +0900

    Initial Commit

---
+++ 2023-2 mainproject_parkingcash/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2023-2 mainproject_parkingcash
+//
+//  Created by 한설 on 2023/08/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
